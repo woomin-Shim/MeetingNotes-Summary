7bcc9254a1d3d11ab6201c5d25deae97578f2520
Woomin <worejj@gmail.com>
2021-10-22 01:01:07 +0900

    클라이언트 부분 구조 잡고 서버에서 데이터 받을 준비

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/meeting',
     proxy({
       target: 'http://localhost:3030',
       changeOrigin: true,
--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express'); // express 사용
-const app = express();
 const bodyParser = require('body-parser'); // body-parser 미들웨어
-var http = require('http');
-const server = require('http').Server(app)
-const io = require('socket.io')(server); 
+const socketio = require('socket.io');
+const cors = require('cors');
+const http = require('http');
+//const server = require('http').Server(app)
+
+const app = express();
+const server = require('http').Server(app);
+const io = socketio(server);
 const PORT = process.env.PORT || 3030;  //3030 port 사용 
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
+if(process.env.NODE_ENV === 'production') {  //AWS서버에서 돌아가면 빌드하기
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  
+  app.get('*', (req, res) => {
+      req.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  })
+};
 
 app.get('/api/load', (req, res) => {
   res.send({ name: '우민', age:27 });
@@ -16,6 +28,4 @@ app.get('/api/load', (req, res) => {
 
 
 // 해당 포트로 서버를 실행
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server has started on port ${port}`));
