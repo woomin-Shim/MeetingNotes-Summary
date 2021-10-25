de6cb94f0a891526b593733ed77593160c3f73b5
Woomin <worejj@gmail.com>
2021-10-26 00:51:31 +0900

    change url when click record button

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Translate from './components/Translate';
 import Home from './components/Home';
-import About from './components/About';
+
 
 const App = () => (
     <Router>
         <Route path="/"   component={Home} />
-        <Route path="/about"  component={About} /> 
     </Router>
 );
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-/**
- * 함수형 컴포넌트 스니펫입니다.
- */
+import logo from '../asset/logo.png';
+import './Home.css';
+
+const Timer = () => {
+  const [visible, setVisible] = useState(true);
+  const [currentHours, setCurrentHours] = useState(0);
+  const [currentMinutes, setCurrentMinutes] = useState(0);
+  const [currentSeconds, setCurrentSeconds] = useState(0);
+
+  let Time = 0;
+  let cron;
+
+  const startButton = () => {
+    updateTimer();
+    cron = setInterval(updateTimer, 1000);
+  };
+
+  const updateTimer = () => {
+    const checkMinutes = Math.floor(Time / 60);
+    const hours = Math.floor(Time / 3600);
+    const minutes = checkMinutes % 60;
+    const seconds = Time % 60;
+    setCurrentHours(hours);
+    setCurrentSeconds(seconds);
+    setCurrentMinutes(minutes);
+    Time++;
+  };
+  return (
+			<div className='recode__button'>
+        
+      <button onClick={() => { setVisible(!visible)  }}>
+        {visible ? <i className='fas fa-microphone fa-2x'></i> : <i className='fas fa-stop fa-2x'></i>}
+      </button>
+
+    	</div>
+  )
+};
 const Home = () => {
   return (
-    <div>
-       <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">어바웃</Link>
-        </li>
-      </ul>
-      <h1>Home</h1>
-      <p>Take me home~ Country road~</p>
+    <div className='App'>
+      <div className='header'>
+        <nav className='navbar'>
+          <div className='navbar__logo'>
+            <img src={logo} alt='123' />
+          </div>
+          <ul className='navbar__menu'>
+            <li>HOME</li>
+            <li>RECORD</li>
+          </ul>
+        </nav>
+        <div className='info'>
+          <div className='info__textbox'>
+            <p className='info__textbox__bigText'>회의록을 간편하게 만들어보세요.</p>
+            <p className='info__textbox__smallText'>
+              사용자가 녹음한 내용을 자동으로 요약하여
+              <br />
+              사용자가 원하는 양식에 맞춰
+              <br />
+              회의록으로 변환해 줍니다.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='recoder'>
+        <div className='recoder__text'>
+          <Link to = "./translate">
+          <button>Record</button>
+          </Link>
+        </div>
+        <div className='recoder__animation'>
+          <div className='loader'>
+            <div className='bar1'></div>
+            <div className='bar2'></div>
+            <div className='bar3'></div>
+            <div className='bar4'></div>
+            <div className='bar5'></div>
+            <div className='bar6'></div>
+          </div>
+        </div>
+        <Timer />
+      </div>
     </div>
   );
 };
 
 export default Home;
+
