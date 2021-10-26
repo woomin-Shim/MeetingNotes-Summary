ad5b556606dce2a5c570c817afbc73a7ee072c83
Woomin <worejj@gmail.com>
2021-10-26 20:30:57 +0900

    record 버튼 다시 누르면 종료되게 해야함

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -88,85 +88,169 @@ const Home = () => {
 /*----------------------------- 음성 인식 -----------------------------------------*/
 
 function SpeechToText () {
+
   try{
-      var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-      var recognition = new SpeechRecognition();
-    }
-    catch(e) {
-      console.error(e);
-      ('.no-browser-support').show();
-      alert('Google Chrome에서만 동작합니다.')
-    }
-    
-    var speechContent = '';
-  
-    /*-----------------------------
-          Voice Recognition 
-    ------------------------------*/
-    // If false, the recording will stop after a few seconds of silence.
-    // When true, the silence period is longer (about 15 seconds),
-    // allowing us to keep recording even when the user pauses. 
-    recognition.continuous = true;
-    recognition.lang = "ko-KR";
-    var recognizing = false;
-  
-    // This block is called every time the Speech APi captures a line. 
-    // 음성 인식 결과 처리
-    recognition.onresult = function(event) {
-      // event is a SpeechRecognitionEvent object.
-      // It holds all the lines we have captured so far. 
-      // We only need the current one.
-      var current = event.resultIndex;
-    
-      // Get a transcript of what was said.
-      var transcript = event.results[current][0].transcript;
-  
-      if(typeof(event.results) == 'undefined'){
-        console.log("undefined start")
-        recognition.stop()
-        recognizing = false
-        recognition.start()
-        console.log("undefined end")
-        return;  
-      }
-  
-      // Add the current transcript to the contents of our Note.
-      // There is a weird bug on mobile, where everything is repeated twice.
-      // There is no official solution so far so we have to handle an edge case.
-      var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
-  
-      if(!mobileRepeatBug) {
-        speechContent += transcript;
-        console.log(speechContent);
-      }
-  
-    };
-  
-    recognition.onstart = function() {
-      console.log('Voice recognition activated.')
-      recognizing = true;
-    }
-  
-    recognition.onend = function () {
-      console.log("ONEND")
-      recognition.stop()
-      recognizing = false
-    }
-  
-    recognition.onerror = function(event) {
-      console.log("ERROR")
-      recognizing = false
-      recognition.stop()
-      if (event.error == 'no-speech') {
-        console.log("NO SPEECH")
-      }
-      if (event.error == 'audio-capture') {
-          console.log("Capture Problem")
-      }
-    }
-    // 음성 인식 트리거
-    recognition.start();
+    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    var recognition = new SpeechRecognition();
   }
+  catch(e) {
+    console.error(e);
+    ('.no-browser-support').show();
+    alert('Google Chrome에서만 동작합니다.')
+  }
+  const language = 'ko-KR';
+  
+  let isRecognizing = false;
+  let ignoreEndProcess = false;
+  let finalTranscript = '';
+
+  var a = '';
+  var translate;
+
+  recognition.continuous = true;
+  recognition.interimResults = true; 
+  //true : recognition이 result의 중간중간을 보고한다
+  
+  /**
+   * 음성 인식 시작 처리
+   */
+  recognition.onstart = function (event) {
+      console.log('onstart', event);
+      isRecognizing = true;
+      //recording_state.className = 'on';
+  };
+
+  /**
+   * 음성 인식 종료 처리
+   */
+  recognition.onend = function () {
+      console.log('onend', arguments);
+      isRecognizing = false;
+
+      if (ignoreEndProcess) {
+          return false;
+      }
+
+      // Do end process
+      console.log('off')
+      if (!finalTranscript) {
+      console.log('empty finalTranscript');
+      return false;
+      }
+  };
+
+  /**
+   * 음성 인식 결과 처리
+   * 밑의 코드는 SpeechRecognition 객체의 onresult Property인 이벤트 핸들러를 설정하는 것임.
+   * API로부터 성공적으로 result를 받았을 때 밑에서 정의한 SpeechRecognition 객체의 onresult 이벤트가 핸들러가 호출됨.
+   */
+  recognition.onresult = function (event) {
+      console.log('onresult', event);
+
+      let finalTranscript = '';
+      let interimTranscript = '';
+      if (typeof event.results === 'undefined') {
+          recognition.onend = null;
+          recognition.stop();
+          return;
+      }
+
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+          const transcript = event.results[i][0].transcript;
+          if (event.results[i].isFinal) {
+              finalTranscript += transcript;
+          } else {
+              interimTranscript += transcript;
+          }
+      }
+
+      console.log('번역중 : ', interimTranscript);
+      console.log('최종번역 : ', finalTranscript);
+      // fireCommand(interimTranscript);
+  };
+
+  /**
+   * 음성 인식 에러 처리
+   */
+  recognition.onerror = function (event) {
+      console.log('onerror', event);
+
+      if (event.error.match(/no-speech|audio-capture|not-allowed/)) {
+      ignoreEndProcess = true;
+      }
+  };
+
+  /**
+   * 명령어 처리
+   * @param string
+   */
+
+  /**
+   * 음성 인식 트리거
+   *  마이크 버튼 눌렀을때
+   */
+  function start() {
+      if (isRecognizing) { //녹음 버튼을 두번 누른거니까 종료
+          recognition.stop();
+          console.log('종료');
+          /* 번역 텍스트 파일 다운로드
+          var hiddenElement = document.createElement('a');
+          hiddenElement.href = 'data:attachment/text,' + encodeURI(recognizedTextarea.innerText);
+          hiddenElement.target = '_blank';
+          hiddenElement.download = 'translate.txt';
+          hiddenElement.click();
+          */
+          /* 소켓 통신
+          socket.emit('translate', {type: 'mymessage', message: translate});
+          */
+          return;
+      }
+      recognition.lang = language;
+      recognition.start(); //이게 호출되면 음성인식 시작
+      a = '';
+      ignoreEndProcess = false;
+
+      finalTranscript = '';
+  }
+  /**
+   * 초기 바인딩
+   */
+  function resultWordHandler(event) {
+      console.log('clicked id : ' + event.target.value);
+  }
+
+  /*
+  function final_span_Handler() {
+      if(final_span.innerHTML) {
+          const final_span_text = final_span.innerHTML; 
+          const final_arr = final_span_text.split(' '); //["안녕하세요", "저는"]
+          
+          let htmlEl = null;
+          final_arr.forEach((value, index) => {
+              if(index === 0) {
+                  htmlEl = `<span class="resultWord" id=0>` + value + '<span/>';
+              }else {
+                  htmlEl = htmlEl + `<span class="resultWord" id=${index}>${value}<span/> ` 
+              }
+          });
+          
+          // console.log('htmlEl : ' + htmlEl);
+          
+          // const resultWord = document.querySelector('.resultWord');
+          // resultWord.addEventListener('click', resultWordHandler);
+
+          final_span.innerHTML = a + htmlEl;
+          a = a + htmlEl + '<br/>';
+          translate = recognizedTextarea.innerHTML; // translate 변수에 총 번역본 저장
+      }else {
+          return null;
+      }
+  }
+  */
+  
+  start();
+}
+
 
 export default Home;
 
