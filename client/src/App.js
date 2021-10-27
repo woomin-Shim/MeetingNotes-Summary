d074d5ac20d88824593730a713fc59f07f3e1d0c
Woomin <worejj@gmail.com>
2021-10-27 19:57:05 +0900

    record 버튼 누르면 녹음되고 텍스트 서버로 전송까지 성공

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home';
-import About from './components/About';
+
 
 
 const App = () => (
     <Router>
         <Route path="/" component={Home} />
-        <Route path="/speechtoText" component={About} />
     </Router>
 );
 
